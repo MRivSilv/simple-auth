e8e3d988b05f9497f8d6aa5453459110b47b9560
MRivSilv <matiasrivillo@gmail.comD>
2026-07-20 20:13:36 -0400

    Added, verification of data at the time of register, also a gen of userid

--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	golang.org/x/crypto v0.54.0
 )
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
--- handlers/register.go
+++ handlers/register.go
@@ -1,19 +1,22 @@
 package handlers
+
 import (
 	"encoding/json"
 	"net/http"
 	"simple-auth/auth"
 	"simple-auth/store"
+	"github.com/google/uuid"
 )
 
-type Credentials struct{
-	Email string `json:"email"`
+type Credentials struct {
+	UserID   string `json:"userid"`
+	Email    string `json:"email"`
 	Username string `json:"username"`
-	Password string	`json:"password"`
+	Password string `json:"password"`
 }
 
-func Register(s *store.UserStore) http.HandlerFunc{
-	return func (w http.ResponseWriter, r *http.Request)  {
+func Register(s *store.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var creds Credentials
 		if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
 			http.Error(w, "server error", http.StatusBadRequest)
@@ -23,10 +26,20 @@ func Register(s *store.UserStore) http.HandlerFunc{
 		if err != nil {
 			http.Error(w, "server error", http.StatusInternalServerError)
 		}
-		if !s.Create(creds.Username, hash){
+		if creds.Email == "" || creds.Password == "" || creds.Username == "" {
+			http.Error(w, "credential is missing", http.StatusBadRequest)
+		}
+		creds.UserID = GenerateUserID()
+		if !s.Create(creds.Username, hash) {
 			http.Error(w, "user exist", http.StatusConflict)
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
 	}
+}
+
+func GenerateUserID() string{
+	id := uuid.New()
+	userID := id.String()
+	return userID
 }
